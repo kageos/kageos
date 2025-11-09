46896766c8100601a99c55222cb5bfbea192e163
liubeiluo <1210227080@qq.com>
2025-11-09 23:06:02 +0800

    fix: 为所有基础组件添加 value prop 默认值
    - InputWidget, NumberWidget, FloatWidget, SwitchWidget, SelectWidget, TimestampWidget
    - 添加 withDefaults 确保 value prop 有默认值
    - 防止组件缺少必需的 value prop 错误

--- web/src/core/widgets-v2/components/SelectWidget.vue
+++ web/src/core/widgets-v2/components/SelectWidget.vue
@@ -83,7 +83,13 @@ import type { WidgetComponentProps, WidgetComponentEmits } from '../types'
 import { useFormDataStore } from '../../stores-v2/formData'
 import { selectFuzzy } from '@/api/function'
 
-const props = defineProps<WidgetComponentProps>()
+const props = withDefaults(defineProps<WidgetComponentProps>(), {
+  value: () => ({
+    raw: null,
+    display: '',
+    meta: {}
+  })
+})
 const emit = defineEmits<WidgetComponentEmits>()
 
 const formDataStore = useFormDataStore()
