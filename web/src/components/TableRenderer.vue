a5772d72ea3929c80c911933467543b1829b7791
beiluo <1210227080@qq.com>
2025-11-22 17:59:39 +0800

    feat: 在打包下载文件名前加上 app 名称
    - 从 router 中提取 app 名称（第二段）
    - 格式：app名称_函数名称_id_记录ID.zip 或 app名称_函数名称_时间戳.zip

--- web/src/components/TableRenderer.vue
+++ web/src/components/TableRenderer.vue
@@ -659,9 +659,11 @@ const renderDetailField = (field: FieldConfig, rawValue: any): any => {
     })
     const recordId = idField && currentDetailRow.value ? currentDetailRow.value[idField.code] : undefined
     
-    // 🔥 从 router 或 currentFunction 获取函数名称
+    // 🔥 从 router 或 currentFunction 获取函数名称和 app 名称
     // router 格式通常是：/user/app/function_name 或 /user/app/group/function_name
     let functionName: string | undefined = undefined
+    let appName: string | undefined = undefined
+    
     if (props.currentFunction?.name) {
       // 优先使用 currentFunction.name
       functionName = props.currentFunction.name
@@ -673,9 +675,23 @@ const renderDetailField = (field: FieldConfig, rawValue: any): any => {
       }
     }
     
+    // 🔥 从 router 中提取 app 名称（通常是第二段，格式：/user/app/...）
+    if (props.functionData?.router) {
+      const routerParts = props.functionData.router.split('/').filter(Boolean)
+      if (routerParts.length >= 2) {
+        appName = routerParts[1]  // 第二段是 app 名称
+      }
+    }
+    
+    // 🔥 如果有 app 名称，在函数名称前面加上
+    if (appName && functionName) {
+      functionName = `${appName}_${functionName}`
+    }
+    
     // 调试日志
     console.log('[TableRenderer] renderDetailField 传递的命名信息:', {
       functionName,
+      appName,
       recordId,
       idField: idField?.code,
       fieldCode: field.code,
