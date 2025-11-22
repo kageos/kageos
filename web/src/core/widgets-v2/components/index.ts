ca4c44d1c0617121d97ff9f5d0f3d3b912e0ab04
liubeiluo <1210227080@qq.com>
2025-11-22 17:20:20 +0800

    feat: 创建 widgets-v2 组件统一导出文件
    - 创建 components/index.ts 统一导出所有组件
    - 包括所有 Widget 组件和通用组件 UserDisplay
    - 方便其他地方导入使用

---
+++ web/src/core/widgets-v2/components/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Widgets-v2 组件统一导出
+ * 方便其他地方导入使用
+ */
+
+// Widget 组件
+export { default as InputWidget } from './InputWidget.vue'
+export { default as NumberWidget } from './NumberWidget.vue'
+export { default as FloatWidget } from './FloatWidget.vue'
+export { default as TextAreaWidget } from './TextAreaWidget.vue'
+export { default as SwitchWidget } from './SwitchWidget.vue'
+export { default as SelectWidget } from './SelectWidget.vue'
+export { default as MultiSelectWidget } from './MultiSelectWidget.vue'
+export { default as CheckboxWidget } from './CheckboxWidget.vue'
+export { default as RadioWidget } from './RadioWidget.vue'
+export { default as TextWidget } from './TextWidget.vue'
+export { default as FilesWidget } from './FilesWidget.vue'
+export { default as TimestampWidget } from './TimestampWidget.vue'
+export { default as FormWidget } from './FormWidget.vue'
+export { default as TableWidget } from './TableWidget.vue'
+export { default as UserWidget } from './UserWidget.vue'
+
+// 通用组件
+export { default as UserDisplay } from './UserDisplay.vue'
+
